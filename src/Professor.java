23ffac6600349b8164bcd61546e037ac5c60e402
Timothy Chu <tchu01@calpoly.edu>
2017-03-07 14:42:06 -0800

    Initial Professor push

---
+++ src/Professor.java
@@ -0,0 +1,19 @@
+/**
+ * Created by timothy on 3/7/17.
+ */
+
+import java.sql.*;
+
+public class Professor {
+
+    int uid;
+    int Professor_ID;
+    String Professor_Name;
+    String Email;
+    String Department;
+
+    public Professor(Connection connect, int Professor_ID, String Professor_Name, String Email, String Department) {
+
+    }
+
+}
