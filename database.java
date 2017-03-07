aa450ad6c4aba8ab0432754c14b319ed9d0d217f
Timothy Chu <tchu01@calpoly.edu>
2017-03-07 12:59:09 -0800

    Added catch

--- database.java
+++ database.java
@@ -12,9 +12,22 @@ class database {
             ResultSet rs;
             Statement statement = connect.createStatement();
             rs = statement.executeQuery("SELECT * FROM Customer");
-            while(rs.next()) {
+            while (rs.next()) {
                 int customerID = rs.getInt(1);
                 System.out.println("Customer id: " + customerID);
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not load JDBC driver");
+            System.err.println("Exception: " + e);
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("SQLException information");
+            while(e != null) {
+                System.err.println("Error msg: " + e.getMessage());
+                System.err.println("SQLSTATE: " + e.getSQLState());
+                System.err.println("Error code: " + e.getErrorCode());
+                e.printStackTrace();
+                e = e.getNextException();
             }
         } catch (Exception e) {
             e.printStackTrace();
